3032eb42c1250c35068b58751e0d5b4e75e93b59
SK207_16 <SK207_16@K207_W816.carrollcc.edu>
2015-11-05 19:13:22 -0500

    Code start

---
+++ Project/src/main/java/com/LeverInc/Project/App.java
@@ -0,0 +1,13 @@
+package com.LeverInc.Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
