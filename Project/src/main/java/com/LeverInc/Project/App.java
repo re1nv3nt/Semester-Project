c61958280d365b6c158e929c74dfe3d167ce240a
kirbyoneill <dkoneill1594@comcast.net>
2015-11-24 19:32:35 -0500

    Very early WebView implementation. Needs to be personalized.

--- Project/src/main/java/com/LeverInc/Project/App.java
+++ Project/src/main/java/com/LeverInc/Project/App.java
@@ -1,13 +1,21 @@
 package com.LeverInc.Project;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App  extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	private Scene scene;
+    @Override public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("Web View");
+        scene = new Scene(new WebBrowserController(),750,500, Color.web("#666970"));
+        stage.setScene(scene);   
+        stage.show();
+    }
+ 
+    public static void main(String[] args){
+        launch(args);
     }
 }
