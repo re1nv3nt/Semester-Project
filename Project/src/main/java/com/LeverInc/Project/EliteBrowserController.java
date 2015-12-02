6ff8fb8d47fb4a71869bc4cc6845a82db72d778c
re1nv3nt <apaczy@gmail.com>
2015-12-01 21:05:42 -0500

    Basic Web Browser working

--- Project/src/main/java/com/LeverInc/Project/EliteBrowserController.java
+++ Project/src/main/java/com/LeverInc/Project/EliteBrowserController.java
@@ -1,21 +1,47 @@
 package com.LeverInc.Project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import javafx.scene.layout.*;
 
-public class EliteBrowserController extends Region{
-
+public class EliteBrowserController {
+	String address = "http://www.google.com";
+	
     @FXML
     private WebView webView;
 
     @FXML
     private TextField tfAddressBar;
-    
+
+    @FXML
+    private Button btnGo;
+
     public void initialize(){
-    WebEngine webEngine = webView.getEngine();
-    webEngine.load("http://www.google.com");
+        WebEngine webEngine = webView.getEngine();
+        tfAddressBar.setText(address);
+        webEngine.load(tfAddressBar.getText());
     }
+    
+    @FXML
+    void onEnterPress(KeyEvent event) {
+    	if(event.getCode() == KeyCode.ENTER){
+    		address = tfAddressBar.getText();
+    		WebEngine webEngine = webView.getEngine();
+    		webEngine.load(addressCorrection());
+    	}
+    }
+    
+    public String addressCorrection(){
+    	if(address.substring(0, 7).equals("http://")){
+    		return address;
+    	} else{
+    		address = "http://" + address;
+    		return address;
+    	}
+    }
+    
 }
