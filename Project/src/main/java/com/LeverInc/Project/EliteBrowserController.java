3880fbbefd868e9ae3cc7b3da1f839faf4a4bf1c
re1nv3nt <apaczy@gmail.com>
2015-12-07 13:12:20 -0500

    Implemented getLocation() for url update, added address bar tooltip, and fixed addressCorrection() method

--- Project/src/main/java/com/LeverInc/Project/EliteBrowserController.java
+++ Project/src/main/java/com/LeverInc/Project/EliteBrowserController.java
@@ -26,37 +26,37 @@ public class EliteBrowserController extends Region{
     private MenuItem close;
 
     public void initialize(){
-    WebEngine webEngine = webView.getEngine();
-    tfAddressBar.setText(address);
-    webEngine.load(address);
+	    WebEngine webEngine = webView.getEngine();
+	    tfAddressBar.setText(address);
+	    webEngine.load(address);
+	    // Address bar helpful tooltip
+	    tfAddressBar.setTooltip(new Tooltip("Enter your destination URL, and may the force be with you"));
     }
     
+    // Corrects user input URL with proper HTML prefix
     public String addressCorrection(){
-    	if(address.startsWith("https://")){
-    		return address;
-    	}else{
-    		address = "http://" + address;
-    	}
-    	
-    	if(address.startsWith("http://")){
+    	if(address.startsWith("http://") || address.startsWith("https://")){
     		return address;
     	} else{
     		address = "http://" + address;
     		return address;
     	}
-    	
     }
     
     @FXML
     void onEnterPress(KeyEvent event) {
     	if(event.getCode() == KeyCode.ENTER){
+    		// Assigns user input text from address bar to "address" String
     		address = tfAddressBar.getText();
     		WebEngine webEngine = webView.getEngine();
+    		// Properly loads new URL with addressCorrection method
     		webEngine.load(addressCorrection());
+    		// Displays the current URL after successful page load
+    		tfAddressBar.setText(webEngine.getLocation());
     	}
     }
 
-
+    // Exits the program
     @FXML
     void browserClose(ActionEvent event) {
     	System.exit(0);
